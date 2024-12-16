1046ee4ff08b35994306c38d9f5861f5f1ad3959
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2024-12-16 18:02:26 +0100

    Implement the basic UI for sending the request

--- DemoOAuthMobiles/App.tsx
+++ DemoOAuthMobiles/App.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
+  Linking,
   StyleSheet,
   View,
 } from 'react-native';
+import { Button } from './components/Button';
 
 function App(): React.JSX.Element {
+  const urlBase = "https://github.com/login/oauth/authorize";
+  const requestParams = {
+  }
+
+  const handlePress = useCallback(async () => {
+      const keys = Object.keys(requestParams);
+      const vals = Object.values(requestParams);
+      let reqParams = "";
+      for (let i = 0; i < keys.length; i++) {
+        reqParams += '&';
+        reqParams += `${keys[i]}=${vals[i]}`;
+      }
+      const reqUrl = urlBase + `?${reqParams}`;
+      console.log("requesting: ", reqUrl);
+      await Linking.openURL(reqUrl);
+  }, [urlBase]);
+
   return (
     <View style={styles.mainContainer}>
+      <Button onPress={handlePress} title='Log In'/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  mainContainer: {}
+  mainContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default App;
