ea9d62352931c414a5be26196a736d900f5a57f8
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2025-01-07 14:24:01 +0100

    Request the access token and parse it

--- DemoOAuthMobiles/App.tsx
+++ DemoOAuthMobiles/App.tsx
@@ -7,10 +7,15 @@ import {
 } from 'react-native';
 import { Button } from './components/Button';
 
+const client_secret = "placeholder";
+const client_id = "placeholder";
+
 function App(): React.JSX.Element {
   const [token, setToken] = useState("");
+  const [accessToken, setAccessToken] = useState("");
   const urlBase = "https://github.com/login/oauth/authorize";
   const requestParams = {
+    client_id: client_id,
     redirect_uri: "DemoOAuthMobiles://code",
   }
 
@@ -27,6 +32,32 @@ function App(): React.JSX.Element {
       await Linking.openURL(reqUrl);
   }, [urlBase]);
 
+  const handlePost = useCallback(() => {
+    fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        client_id: client_id,
+        client_secret: client_secret,
+        response_type: "token",
+        code: token,
+        redirect_uri: "DemoOAuthMobiles://code"
+      }),
+    }).then((response) => {
+      response.json().then(res => {
+        console.log("The response is: ", JSON.stringify(res));
+        setAccessToken(res.access_token);
+      }).catch(error => {
+        console.log("cant open response, error: ", error);
+      });
+    }).catch(error => {
+      console.log("The error is: ", error);
+    });
+  }, [token]);
+
       // Listen to incoming links from deep linking
   Linking.addEventListener('url', ({ url }) => {
     console.log("URL received: ", url);
@@ -36,7 +67,13 @@ function App(): React.JSX.Element {
   return (
     <View style={styles.mainContainer}>
       <Button onPress={handlePress} title='Log In'/>
-      <View style={styles.tokenResultContainer}><Text>Token is:</Text><Text>{token}</Text></View>
+      <View style={styles.tokenResultContainer}>
+        <Text>Token is:</Text>
+        <Text>{token}</Text>
+        <Text>Access token is:</Text>
+        <Text>{accessToken}</Text>
+      </View>
+      {token ? <Button onPress={handlePost} title='Continue' /> : null}
     </View>
   );
 }
